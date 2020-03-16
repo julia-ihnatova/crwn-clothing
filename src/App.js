a5042c7d79ed39daa0b855f82e2c664d30fdbdc7
julia <julia.ignatova11@gmail.com>
2020-03-16 08:07:00 +0200

    updating our application with reselect, replaced cart selectors

--- src/App.js
+++ src/App.js
@@ -1,73 +1,75 @@
 import React from 'react';
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import Header from "./components/header/header.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { setCurrentUser } from "./redux/user/user.actions";
 
-class App extends React.Component{
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
-    unsubscribeFromAuth = null;
+class App extends React.Component {
+  unsubscribeFromAuth = null;
 
-    componentDidMount(){
-        const { setCurrentUser } = this.props;
-        //onAuthStateChanged gives back a function, if we call it close subscription
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-            if (userAuth) {
-                const userRef = await createUserProfileDocument(userAuth);
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
 
-                userRef.onSnapshot(snapShot => {
-                    setCurrentUser({
-                            id: snapShot.id,
-                            ...snapShot.data()
-                    }, () => {
-                        // console.log(this.state);
-                    });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-                });
-            }
-
-            setCurrentUser(userAuth);
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
         });
-    }
+      }
 
-    componentWillUnmount() {
-        this.unsubscribeFromAuth();
-    }
+      setCurrentUser(userAuth);
+    });
+  }
 
-    render(){
-        return (
-            <div>
-                <Header/>
-                <Switch>
-                    <Route exact path='/' component={HomePage}/>
-                    <Route path='/shop' component={ShopPage}/>
-                    <Route exact path='/signin'
-                           render={() =>
-                               this.props.currentUser ? (
-                                   <Redirect to='/' />
-                                   ) : (
-                                  <SignInAndSignUpPage/>
-                                  )}/>
-                </Switch>
-            </div>
-        );
-    }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = ({user}) => ({
-    currentUser: user.currentUser,
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
 });
 
 const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch(setCurrentUser((user)))
-
+  setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,45 +1,44 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyA3-SC_uVXp1JAG3Msn-emb7GUkubh-kSM",
-    authDomain: "crwn-db-cd09a.firebaseapp.com",
-    databaseURL: "https://crwn-db-cd09a.firebaseio.com",
-    projectId: "crwn-db-cd09a",
-    storageBucket: "crwn-db-cd09a.appspot.com",
-    messagingSenderId: "891269009320",
-    appId: "1:891269009320:web:f451968806716728d6d79d",
-    measurementId: "G-8L2LEGN8KK"
+  apiKey: "AIzaSyA3-SC_uVXp1JAG3Msn-emb7GUkubh-kSM",
+  authDomain: "crwn-db-cd09a.firebaseapp.com",
+  databaseURL: "https://crwn-db-cd09a.firebaseio.com",
+  projectId: "crwn-db-cd09a",
+  storageBucket: "crwn-db-cd09a.appspot.com",
+  messagingSenderId: "891269009320",
+  appId: "1:891269009320:web:f451968806716728d6d79d",
+  measurementId: "G-8L2LEGN8KK"
 };
 
-//take authenticated with google user object and store it inside our database
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
-
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-
-    const snapShot = await userRef.get();
-
-    if (!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            });
-        } catch (error) {
-            console.log('error creating user', error.message);
-        }
-    }
-
-    return userRef;
-}
-
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
